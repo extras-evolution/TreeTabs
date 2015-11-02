f53e537c442821b682288d7a415cbbfd40d40b81
Lukianenko Dmytro <dmi3yy@MacBook-Pro-Lukianenko.local>
2015-11-02 20:41:09 +0200

    mysql_ to $modx db

--- assets/plugins/treetabs/actions/chunk.php
+++ assets/plugins/treetabs/actions/chunk.php
@@ -16,8 +16,8 @@ $tbl_site_htmlsnippets = $modx->getFullTableName('site_htmlsnippets');
 $content = array();
 if (isset($_REQUEST['id']) && $_REQUEST['id']!='' && is_numeric($_REQUEST['id'])) {
     $sql = 'SELECT * FROM '.$tbl_site_htmlsnippets.' WHERE id=\''.$id.'\'';
-    $rs = mysql_query($sql);
-    $limit = mysql_num_rows($rs);
+    $rs = $modx->db->query($sql);
+    $limit = $modx->db->getRecordCount($rs);
     if ($limit > 1) {
         echo '<p>Error: Multiple Chunk sharing same unique ID.</p>';
         exit;
@@ -26,7 +26,7 @@ if (isset($_REQUEST['id']) && $_REQUEST['id']!='' && is_numeric($_REQUEST['id'])
         echo '<p>Chunk doesn\'t exist.</p>';
         exit;
     }
-    $content = mysql_fetch_assoc($rs);
+    $content = $modx->db->getRow($rs);
     $_SESSION['itemname'] = $content['name'];
     if ($content['locked'] == 1 && $_SESSION['mgrRole'] != 1) {
         $e->setError(3);
